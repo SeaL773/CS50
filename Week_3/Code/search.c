e5bb760b6e61a734cc3bd01901d6b83b318d81fd
SeaL773 <wanglizhoujerry@163.com>
2023-12-16 11:21:38 -0500

    Update search.c

--- Week_3/Code/search.c
+++ Week_3/Code/search.c
@@ -1,7 +1,8 @@
 #include "cs50.h"
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
+/* int main(void)
 {
     // An array of integers
     int numbers[] = {20, 500, 10, 5, 100, 1, 50};
@@ -11,6 +12,29 @@ int main(void)
     for (int i = 0; i < 7; i++)
     {
         if (numbers[i] == n)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+} */
+
+
+int main(void)
+{
+    // An array of strings
+    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
+
+    // Calculate the length of the array
+    int length = sizeof(strings) / sizeof(string);
+
+    // Search for string
+    string s = get_string("String: ");
+    for (int i = 0; i < length; i++)
+    {
+        if (strcmp(strings[i], s) == 0)
         {
             printf("Found\n");
             return 0;
