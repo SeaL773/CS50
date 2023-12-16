7023eaacdf7dea399eb6ec8777d3e58bbb78fb31
SeaL773 <wanglizhoujerry@163.com>
2023-12-16 11:31:08 -0500

    Update phonebook.c

--- Week_3/Code/phonebook.c
+++ Week_3/Code/phonebook.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
+/* int main(void)
 {
     // Arrays of strings
     string names[] = {"Carter", "David"};
@@ -15,6 +15,38 @@ int main(void)
         if (strcmp(names[i], name) == 0)
         {
             printf("Found %s\n", numbers[i]);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+} */
+
+// Like map or multi-array in Java?
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+
+int main(void)
+{
+    person people[2];
+
+    people[0].name = "Carter";
+    people[0].number = "+1-617-495-1000";
+
+    people[1].name = "David";
+    people[1].number = "+1-949-468-2750";
+
+    // Search for name
+    string name = get_string("Name: ");
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(people[i].name, name) == 0)
+        {
+            printf("Found %s\n", people[i].number);
             return 0;
         }
     }
