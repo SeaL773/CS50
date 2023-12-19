b56796e3b009578574da173f8694dedbfb9f59b7
SeaL773 <wanglizhoujerry@163.com>
2023-12-19 14:00:59 -0500

    Update recover

--- Week_4/Problem_Set/recover/recover.c
+++ Week_4/Problem_Set/recover/recover.c
@@ -1,7 +1,62 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
+
+typedef uint8_t BYTE;
+
+#define BLOCK_SIZE 512
+#define FILE_NAME_SIZE 8 // 3 digits + ".jpg" + null terminator
+
 int main(int argc, char *argv[])
 {
-    
+    if (argc != 2)
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
+
+    char *file = argv[1];
+    FILE *raw_file = fopen(file, "r");
+    if (raw_file == NULL)
+    {
+        printf("Could not open %s.\n", file);
+        return 2;
+    }
+
+    BYTE buffer[BLOCK_SIZE];
+    FILE *img = NULL;
+    char filename[FILE_NAME_SIZE];
+    int filecount = 0;
+
+    while (fread(buffer, BLOCK_SIZE, 1, raw_file) == 1)
+    {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            if (img != NULL)
+            {
+                fclose(img);
+            }
+
+            sprintf(filename, "%03i.jpg", filecount++);
+            img = fopen(filename, "w");
+            if (img == NULL)
+            {
+                printf("Could not create %s.\n", filename);
+                return 3;
+            }
+        }
+
+        if (img != NULL)
+        {
+            fwrite(buffer, BLOCK_SIZE, 1, img);
+        }
+    }
+
+    if (img != NULL)
+    {
+        fclose(img);
+    }
+
+    fclose(raw_file);
+    return 0;
 }
