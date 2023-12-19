c30af5cffe2bfe276e7c942d9a2368ea209e205d
SeaL773 <wanglizhoujerry@163.com>
2023-12-18 20:42:35 -0500

    Update Lab

---
+++ Week_4/Lab/volume/volume.c
@@ -0,0 +1,57 @@
+// Modifies the volume of an audio file
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    // Note: The input file should be opened using "rb" (read binary) mode
+    // This is because WAV files are binary files, not text files.
+    // Not 'r'
+    FILE *input = fopen(argv[1], "rb");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    // Note: The output file should be opened using "wb" (write binary) mode
+    // Not 'w'
+    FILE *output = fopen(argv[2], "wb");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    // TODO: Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
+
+    // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    while (fread(&buffer, sizeof(int16_t), 1, input))
+    {
+        buffer *= factor;
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
+
+    // Close files
+    fclose(input);
+    fclose(output);
+}
