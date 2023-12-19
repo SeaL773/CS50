784d0371b08460af045684ae0e6daca712df4797
SeaL773 <wanglizhoujerry@163.com>
2023-12-19 01:54:54 -0500

    Update filter-more

--- Week_4/Problem_Set/filter-more/helpers.c
+++ Week_4/Problem_Set/filter-more/helpers.c
@@ -15,29 +15,6 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
-// Convert image to sepia
-void sepia(int height, int width, RGBTRIPLE image[height][width])
-{
-    for (int row = 0; row < height; row++)
-    {
-        for (int column = 0; column < width; column++)
-        {
-            int originalRed = image[row][column].rgbtRed;
-            int originalGreen = image[row][column].rgbtGreen;
-            int originalBlue = image[row][column].rgbtBlue;
-
-            int sepiaRed = fmin(round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue), 255);
-            int sepiaGreen = fmin(round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue), 255);
-            int sepiaBlue = fmin(round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue), 255);
-
-            image[row][column].rgbtRed = sepiaRed;
-            image[row][column].rgbtGreen = sepiaGreen;
-            image[row][column].rgbtBlue = sepiaBlue;
-        }
-    }
-    return;
-}
-
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -56,5 +33,98 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE temp[height][width];
+
+    int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            int sumRedX = 0, sumGreenX = 0, sumBlueX = 0;
+            int sumRedY = 0, sumGreenY = 0, sumBlueY = 0;
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    int newRow = row + i;
+                    int newColumn = column + j;
+
+                    if (newRow >= 0 && newRow < height && newColumn >= 0 && newColumn < width)
+                    {
+                        RGBTRIPLE pixel = image[newRow][newColumn];
+
+                        sumRedX += pixel.rgbtRed * Gx[i + 1][j + 1];
+                        sumGreenX += pixel.rgbtGreen * Gx[i + 1][j + 1];
+                        sumBlueX += pixel.rgbtBlue * Gx[i + 1][j + 1];
+
+                        sumRedY += pixel.rgbtRed * Gy[i + 1][j + 1];
+                        sumGreenY += pixel.rgbtGreen * Gy[i + 1][j + 1];
+                        sumBlueY += pixel.rgbtBlue * Gy[i + 1][j + 1];
+                    }
+                }
+            }
+
+            temp[row][column].rgbtRed = fmin(round(sqrt(sumRedX * sumRedX + sumRedY * sumRedY)), 255);
+            temp[row][column].rgbtGreen = fmin(round(sqrt(sumGreenX * sumGreenX + sumGreenY * sumGreenY)), 255);
+            temp[row][column].rgbtBlue = fmin(round(sqrt(sumBlueX * sumBlueX + sumBlueY * sumBlueY)), 255);
+        }
+    }
+
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            image[row][column] = temp[row][column];
+        }
+    }
+    
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE temp[height][width];
+
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            int sumRed = 0, sumGreen = 0, sumBlue = 0;
+            int pixelCount = 0;
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    int newRow = row + i;
+                    int newColumn = column + j;
+
+                    if (newRow >= 0 && newRow < height && newColumn >= 0 && newColumn < width)
+                    {
+                        sumRed += image[newRow][newColumn].rgbtRed;
+                        sumGreen += image[newRow][newColumn].rgbtGreen;
+                        sumBlue += image[newRow][newColumn].rgbtBlue;
+                        pixelCount++;
+                    }
+                }
+            }
+
+            temp[row][column].rgbtRed = round((float) sumRed / pixelCount);
+            temp[row][column].rgbtGreen = round((float) sumGreen / pixelCount);
+            temp[row][column].rgbtBlue = round((float) sumBlue / pixelCount);
+        }
+    }
+
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            image[row][column] = temp[row][column];
+        }
+    }
     return;
 }
